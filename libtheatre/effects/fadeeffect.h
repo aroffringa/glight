e496c1ac3aecf7b9bf325b7b86d179884ea4783d
André Offringa <offringa@gmail.com>
2019-05-18 23:06:25 +0200

    Change fade speed units and default value
    Fade speed units are now in fade/second, and the default fade speed is 1 fade/second

--- libtheatre/effects/fadeeffect.h
+++ libtheatre/effects/fadeeffect.h
@@ -10,8 +10,8 @@ public:
 	FadeEffect() :
 		Effect(1),
 		_fadingValue(0),
-		_fadeUpSpeed(0.0),
-		_fadeDownSpeed(0.0),
+		_fadeUpSpeed(1.0),
+		_fadeDownSpeed(1.0),
 		_previousTime(0.0)
 	{ }
 	
@@ -26,7 +26,7 @@ public:
 protected:
 	virtual void mix(const ControlValue* values, unsigned* channelValues, unsigned universe, const Timing& timing) final override
 	{
-		double timePassed = timing.TimeInMS() - _previousTime;
+		double timePassed = 0.001*(timing.TimeInMS() - _previousTime);
 		_previousTime = timing.TimeInMS();
 		unsigned targetValue = values[0].UInt();
 		if(targetValue != _fadingValue)
