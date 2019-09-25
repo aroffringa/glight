1e2d58dcb1872c8a886dbc0dca2c859b94b29b9c
André Offringa <offringa@gmail.com>
2019-09-25 21:27:30 +0200

    Add constant value effect
    Fixes #188.

---
+++ theatre/effects/constantvalueeffect.h
@@ -0,0 +1,28 @@
+#ifndef CONSTANT_VALUE_EFFECT_H
+#define CONSTANT_VALUE_EFFECT_H
+
+#include "../effect.h"
+
+class ConstantValueEffect : public Effect
+{
+public:
+	ConstantValueEffect() :
+		Effect(0),
+		_value(ControlValue::MaxUInt()) // 2 %
+	{ }
+	
+	virtual Effect::Type GetType() const override { return ConstantValueType; }
+	
+	unsigned Value() const { return _value; }
+	void SetValue(unsigned value) { _value = value; }
+	
+protected:
+	virtual void mix(const ControlValue* values, unsigned* channelValues, unsigned universe, const class Timing& timing) final override
+	{
+		setConnectedInputs(ControlValue(_value));
+	}
+private:
+	unsigned _value;
+};
+
+#endif
