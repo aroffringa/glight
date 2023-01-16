c7c0061cc7b3a6143e28f7739fd03e8f29f28694
André Offringa <offringa@gmail.com>
2023-01-16 20:58:46 +0100

    Add missing file

---
+++ tests/tscene.cpp
@@ -0,0 +1,27 @@
+#include "../theatre/management.h"
+#include "../theatre/scene.h"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace glight::theatre;
+
+BOOST_AUTO_TEST_SUITE(scene)
+
+BOOST_AUTO_TEST_CASE(make) {
+  Management management;
+  Scene& scene = management.AddScene(true);
+  BOOST_CHECK_EQUAL(management.Controllables().size(), 1);
+  KeySceneItem& item = *scene.AddKeySceneItem(1250);
+  BOOST_CHECK_EQUAL(scene.SceneItems().begin()->first, 1250);
+  BOOST_CHECK(scene.SceneItems().begin()->second.get() == &item);
+  item.SetDurationInMS(750);
+  BOOST_CHECK_EQUAL(item.OffsetInMS(), 1250);
+  BOOST_CHECK_EQUAL(item.DurationInMS(), 750);
+  scene.ChangeSceneItemStartTime(&item, 2000);
+  BOOST_CHECK_EQUAL(scene.SceneItems().begin()->first, 2000);
+  BOOST_CHECK_EQUAL(item.OffsetInMS(), 2000);
+  scene.Remove(&item);
+  BOOST_CHECK(scene.SceneItems().empty());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
