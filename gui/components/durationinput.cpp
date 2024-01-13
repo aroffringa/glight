2aa05d597858590592f2129cd7dc88f0996effe9
André Offringa <offringa@gmail.com>
2024-01-13 14:21:38 +0100

    Fix crash when selecting highest beat count (#382)

--- gui/components/durationinput.cpp
+++ gui/components/durationinput.cpp
@@ -4,22 +4,22 @@
 
 namespace glight::gui {
 namespace {
-inline constexpr size_t NVALUES = 21;
-inline const double values[NVALUES] = {
+inline constexpr size_t kNValues = 21;
+inline const double values[kNValues] = {
     0.0,    40.0,    80.0,    160.0,   300.0,   500.0,    750.0,
     1000.0, 1500.0,  2000.0,  2500.0,  3000.0,  4000.0,   5000.0,
     7500.0, 10000.0, 15000.0, 30000.0, 60000.0, 120000.0, 300000.0};
 }  // namespace
 
 DurationInput::DurationInput(double value)
-    : scale_(Gtk::Adjustment::create(0, 0, NVALUES, 1),
+    : scale_(Gtk::Adjustment::create(0, 0, kNValues, 1),
              Gtk::ORIENTATION_HORIZONTAL) {
   Initialize(value);
 }
 
 DurationInput::DurationInput(const std::string &label, double value)
     : label_(label),
-      scale_(Gtk::Adjustment::create(0, 0, NVALUES, 1),
+      scale_(Gtk::Adjustment::create(0, 0, kNValues, 1),
              Gtk::ORIENTATION_HORIZONTAL) {
   label_.set_halign(Gtk::ALIGN_END);
   pack_start(label_, false, false);
@@ -45,7 +45,7 @@ void DurationInput::Initialize(double value) {
 }
 
 double DurationInput::ValueToScale(double value) {
-  for (size_t i = 0; i != NVALUES - 1; ++i) {
+  for (size_t i = 0; i != kNValues - 1; ++i) {
     if (values[i + 1] > value) {
       if (value - values[i] < values[i + 1] - value)
         return i;
@@ -53,7 +53,7 @@ double DurationInput::ValueToScale(double value) {
         return i + 1;
     }
   }
-  return NVALUES - 1;
+  return kNValues - 1;
 }
 
 void DurationInput::onScaleChanged() {
