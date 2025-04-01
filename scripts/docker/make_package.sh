13e4e0e675fc125b71f7d496c53ca8286125746b
André Offringa <offringa@astron.nl>
2025-04-01 21:33:14 +0200

    Set version to 0.9.2

--- scripts/docker/make_package.sh
+++ scripts/docker/make_package.sh
@@ -8,13 +8,13 @@ glightdir=`realpath ${scriptdir}/../..`
 
 sudo docker build -t glight-package-ubuntu24 -f Ubuntu24-build-package ${glightdir}
 mkdir -p ${glightdir}/build/package
-sudo docker run --mount src=${glightdir}/build/package,target=/package,type=bind glight-package-ubuntu24 /usr/bin/bash -c "mv /build/glight-0.9.0-Linux.deb /package/glight-0.9.0-Ubuntu24.deb"
+sudo docker run --mount src=${glightdir}/build/package,target=/package,type=bind glight-package-ubuntu24 /usr/bin/bash -c "mv /build/glight-0.9.2-Linux.deb /package/glight-0.9.2-Ubuntu24.deb"
 echo Package was written to ${glightdir}/build/package/
 sudo docker build -f Ubuntu24-test-deb ../..
 
 sudo docker build -t glight-package-ubuntu22 -f Ubuntu22-build-package ${glightdir}
 mkdir -p ${glightdir}/build/package
-sudo docker run --mount src=${glightdir}/build/package,target=/package,type=bind glight-package-ubuntu22 /usr/bin/bash -c "mv /build/glight-0.9.0-Linux.deb /package/glight-0.9.0-Ubuntu22.deb"
+sudo docker run --mount src=${glightdir}/build/package,target=/package,type=bind glight-package-ubuntu22 /usr/bin/bash -c "mv /build/glight-0.9.2-Linux.deb /package/glight-0.9.2-Ubuntu22.deb"
 echo Package was written to ${glightdir}/build/package/
 sudo docker build -f Ubuntu22-test-deb ../..
 
